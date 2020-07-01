aacd01448d3dfbe501f6dc9818c0d158adb99fbb
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-07-01 23:46:47 +0100

    Dictionary of links achieved, now generate PDFs

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from links import get_links
+from links import get_dict_of_links, read_list_of_links
 
 
 def main():
-    get_links()
+    dict_of_links = get_dict_of_links(read_list_of_links('links.txt'))
+    print()
 
 
 if __name__ == '__main__':
