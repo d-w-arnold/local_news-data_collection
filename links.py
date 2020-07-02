2ff05402bbc44f19c7aef4729295568cbba224f7
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-07-02 18:16:36 +0100

    Reduced scope of BeautifulSoup objects by looking at webpage containers/content-containers instead of entire webpage

--- links.py
+++ links.py
@@ -8,21 +8,54 @@ def read_list_of_links(path):
     links = file1.readlines()
     tmp = list()
     for x in links:
-        if re.search('^#', x) is None:  # Use '#' at the beginning of a line for line comment in links.txt
+        # Use '#' at the beginning of a line for line comment in links.txt
+        if re.search('^#', x) is None:
             tmp.append(x.rstrip("\n"))
     return tmp
 
 
+def get_soup(url_proto_domain, html_page):
+    print("Domain is: {}".format(url_proto_domain))
+    if url_proto_domain == 'https://www.bbc.co.uk':
+        return BeautifulSoup(html_page, "html.parser").find("div", {"id": "site-container"}).findAll('a')
+    elif url_proto_domain == 'https://www.kentonline.co.uk':
+        return BeautifulSoup(html_page, "html.parser").find("div", {"class": "MainSite"}).findAll('a')
+    elif url_proto_domain == 'https://www.cambridgeindependent.co.uk':
+        return BeautifulSoup(html_page, "html.parser").find("div", {"id": "EWrap"}).findAll('a')
+    elif url_proto_domain == 'https://www.bristolpost.co.uk':
+        return BeautifulSoup(html_page, "html.parser").find("main").findAll('a')
+    elif url_proto_domain == 'https://www.nottinghampost.com':
+        return BeautifulSoup(html_page, "html.parser").find("main").findAll('a')
+    elif url_proto_domain == 'https://www.thestar.co.uk':
+        return BeautifulSoup(html_page, "html.parser").find("div", {"id": "frameInner"}).findAll('a')
+    elif url_proto_domain == 'https://www.miltonkeynes.co.uk':
+        return BeautifulSoup(html_page, "html.parser").find("div", {"id": "frameInner"}).findAll('a')
+    elif url_proto_domain == 'http://altrincham.today':
+        return BeautifulSoup(html_page, "html.parser").find("main").findAll('a')
+    elif url_proto_domain == 'https://onthewight.com':
+        return BeautifulSoup(html_page, "html.parser").find("div", {"id": "content"}).findAll('a')
+    elif url_proto_domain == 'https://thelincolnite.co.uk':
+        return BeautifulSoup(html_page, "html.parser").find("main").findAll('a')
+    elif url_proto_domain == 'https://theisleofthanetnews.com':
+        return BeautifulSoup(html_page, "html.parser").find("div", {"class": "mh-wrapper"}).findAll('a')
+    elif url_proto_domain == 'https://westbridgfordwire.com':
+        return BeautifulSoup(html_page, "html.parser").find("div", {"class": "td-main-content-wrap"}).findAll('a')
+    elif url_proto_domain == 'https://www.yourthurrock.com':
+        return BeautifulSoup(html_page, "html.parser").find("div", {"class", "container"}).findAll("a")
+    else:
+        return BeautifulSoup(html_page, "html.parser").findAll('a')
+
+
 def get_dict_of_links(links):
     dict_of_links = dict()
+    total_links = 0
     for x in links:
         try:
             url_proto_domain = re.search('(http[s]?://)?([^/\s]+)', x).group(0)
             req = Request(x)
             html_page = urlopen(req)
-            soup = BeautifulSoup(html_page, "html.parser")
             links = []
-            for link in soup.findAll('a'):
+            for link in get_soup(url_proto_domain, html_page):
                 url_path = str(link.get('href'))
                 if re.search('^(http|https)://', url_path) is not None:
                     links.append(url_path)
@@ -31,7 +64,10 @@ def get_dict_of_links(links):
             print("{0} - {1} links".format(x, len(links)))
             print()
             dict_of_links[x] = links
+            total_links += len(links)
         except Exception as e:
-            print("Exception: {0} - {1}".format(x, e))
+            print("** Exception ** : {0} - {1}".format(x, e))
             print()
+    print("** Total number of links ** : {}".format(total_links))
+    print()
     return dict_of_links
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from links import get_dict_of_links, read_list_of_links
+from links import read_list_of_links, get_dict_of_links
 from pdfs import gen_pdfs
 
 
 def main():
     list_of_links = read_list_of_links('links.txt')
     dict_of_links = get_dict_of_links(list_of_links)
-    gen_pdfs(list_of_links, dict_of_links)
+    # gen_pdfs(list_of_links, dict_of_links)
 
 
 if __name__ == '__main__':
