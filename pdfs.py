ed44541927e6bbddd49c5d8956a4123fa7c0c3ec
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-07-02 21:43:55 +0100

    Improved print messages in pdfs.py, now starts with empty pdfs directory and removed queries from pdf name

--- pdfs.py
+++ pdfs.py
@@ -1,6 +1,7 @@
 import os
 import re
 import shutil
+from urllib.parse import urljoin, urlparse
 
 
 def gen_command(print_to_pdf_path, link):
@@ -15,35 +16,38 @@ def gen_master_pdf(link):
 
 
 def gen_link_pdf(path_for_lnk_pdfs, link):
-    pdf_filename = str("<" + str(link).replace("://", "_").replace("/", "_") + ">.pdf")
+    pdf_filename = str("<" + str(urljoin(link, urlparse(link).path)).replace("://", "_").replace("/", "_") + ">.pdf")
     print_to_pdf_path = os.path.join(path_for_lnk_pdfs, pdf_filename)
     return os.system(gen_command(print_to_pdf_path, link))
 
 
-def del_dir_contents(path_for_lnk_pdfs):
-    for filename in os.listdir(path_for_lnk_pdfs):
-        file_path = os.path.join(path_for_lnk_pdfs, filename)
+def del_dir_contents(path):
+    print("Deleting files/directories in: {}".format(path))
+    for filename in os.listdir(path):
+        file_path = os.path.join(path, filename)
         try:
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
-            print("Emptied directory: {}".format(path_for_lnk_pdfs))
+            print("Deleted file: {}".format(file_path))
         except Exception as e:
             print("Failed to delete: {0} - Reason: {1}".format(file_path, e))
 
 
 def gen_pdfs(lol, dol):
+    print("** Generating PDFs using list of URLs and dictionary of URLs **")
+    pdfs_path = os.path.join(os.getcwd(), "pdfs")
+    if os.path.isdir(pdfs_path):
+        del_dir_contents(pdfs_path)
+    else:
+        os.mkdir(pdfs_path)
     for lnk in lol:
+        print("** Generating PDFs for: {} **".format(lnk))
         gen_master_pdf(lnk)
         url_domain = re.search('https?://([A-Za-z_0-9.-]+).*', lnk).group(1)
-        path_for_lnk_pdfs = os.path.join(os.getcwd(), "pdfs", url_domain)
-        if os.path.isdir(path_for_lnk_pdfs):
-            # Delete contents of directory
-            del_dir_contents(path_for_lnk_pdfs)
-        else:
-            # Create directory
-            os.mkdir(path_for_lnk_pdfs)
-            print("Created directory: {}".format(path_for_lnk_pdfs))
+        path_for_lnk_pdfs = os.path.join(os.getcwd(), "pdfs", "<" + url_domain + ">")
+        os.mkdir(path_for_lnk_pdfs)
+        print("Created directory: {}".format(path_for_lnk_pdfs))
         for link in dol[lnk]:
             gen_link_pdf(path_for_lnk_pdfs, link)
